1d27a860ba27d7e26b0544bc292f1cbf1d21f173
Angelika Kachur <angelika.kachur@gmail.com>
2017-10-13 21:07:21 +0300

    Init

---
+++ src/js/main.js
@@ -0,0 +1,14 @@
+/*
+ Third party
+ */
+//= ../../bower_components/jquery/dist/jquery.min.js
+
+/*
+    Custom
+ */
+//= partials/jquery-3.2.0.min.js
+//= partials/burger-menu.js
+//= partials/helper.js
+//= partials/slick.js
+//= partials/initialization.js
+
---
+++ src/js/partials/initialization.js
@@ -0,0 +1,16 @@
+// $(document).ready(function(){
+
+//   $('.home-slider').slick({
+//     autoplay: true,
+//   	autoplaySpeed: 2000,
+//   	slidesToShow: 4, 
+//   	slidesToScroll: 4,
+//     adaptiveHeight: true, 
+//     responsive: [
+//       { breakpoint: 768,
+//         settings: "unslick"
+//       }
+//     ]
+//   });
+
+// });
